f328c2fb9d72a6ffa3ff28feeefc9979218b1287
progwired <progwired@gmail.com>
2024-02-25 02:45:10 +0530

    initializing the code with docker

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PW: Home Page",
+  description: "PW is one stop for code learning",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col justify-between p-4">{children}</main>
+      </body>
+    </html>
+  );
+}
